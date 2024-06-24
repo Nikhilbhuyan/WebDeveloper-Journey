7c5d04200887b81c9a079cda3f097ee5db8fc9b9
Nikhil Bhuyan <nikhilbhuyan17@gmail.com>
2024-06-24 22:20:51 +0530

    Added jwt and user array

--- Authentication(24.6.24)/Asg1/asg1.js
+++ Authentication(24.6.24)/Asg1/asg1.js
@@ -12,4 +12,26 @@
 
 */
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const app = express();
+
+const jwtPassword = "12345";
+
+const All_USERS = [
+    {
+        username: "ronaldo@gmail.com",
+        password:"cr7",
+        name: "Cristiano Ronaldo"
+    },
+    {
+        username: "nikhil@gmail.com",
+        password: "nikhil@17",
+        name: "Nikhil Bhuyan"
+    },
+    {
+        username: "nihar@gmail.com",
+        password: "12345",
+        name: "Nihar Ranjan Bhuyan"
+    }
+]
+
